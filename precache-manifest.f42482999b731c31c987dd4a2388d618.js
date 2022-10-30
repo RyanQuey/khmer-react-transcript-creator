151bc286416fb5dfa889652bd44e8122bf90a2e6
Jason Liw <jasonl@canadahelps.org>
2022-10-30 12:42:10 -0400

    Updates

---
+++ precache-manifest.f42482999b731c31c987dd4a2388d618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78cf6efd62485da4ceed",
+    "url": "./static/css/main.3927d80d.chunk.css"
+  },
+  {
+    "revision": "78cf6efd62485da4ceed",
+    "url": "./static/js/main.78cf6efd.chunk.js"
+  },
+  {
+    "revision": "cf045ffaa5623248e4d8",
+    "url": "./static/js/1.cf045ffa.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4520a44526b67003e6127ded7584a769",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.138e94109af9dfc1f8268fe4bfb41cce.js"
+  "./precache-manifest.f42482999b731c31c987dd4a2388d618.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
