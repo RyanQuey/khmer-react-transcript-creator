29816996514da6d8de60da6b0d8c4e66e979be9e
Jason Liw <jasonl@canadahelps.org>
2022-10-30 21:24:38 -0400

    Updates

---
+++ precache-manifest.3f250c7b25147fb8e617ddfc1ea4e4aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d620129566b9e8f323f3",
+    "url": "./static/css/main.3927d80d.chunk.css"
+  },
+  {
+    "revision": "d620129566b9e8f323f3",
+    "url": "./static/js/main.d6201295.chunk.js"
+  },
+  {
+    "revision": "c748b78c1ce22e0181f1",
+    "url": "./static/js/1.c748b78c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "463b0d1df35346adbf25c878a3d8bfb6",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f42482999b731c31c987dd4a2388d618.js"
+  "./precache-manifest.3f250c7b25147fb8e617ddfc1ea4e4aa.js"
 );
 
 workbox.clientsClaim();
