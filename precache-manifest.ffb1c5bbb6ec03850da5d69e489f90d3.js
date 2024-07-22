49d27ac36b3f098e574dcbc7448b3ee79e1c626b
Ryan Quey <quey.ryan@gmail.com>
2024-07-22 14:11:48 +0700

    Updates

---
+++ precache-manifest.ffb1c5bbb6ec03850da5d69e489f90d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16981de9c2adaf01afd8028802e06833",
+    "url": "./static/media/Khmer Mondulkiri Regular.16981de9.ttf"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "967a47283c0e5c6d63d6",
+    "url": "./static/js/main.967a4728.chunk.js"
+  },
+  {
+    "revision": "d0408911ffe740f73b9b",
+    "url": "./static/js/1.d0408911.chunk.js"
+  },
+  {
+    "revision": "967a47283c0e5c6d63d6",
+    "url": "./static/css/main.8fe825b2.chunk.css"
+  },
+  {
+    "revision": "a3825880db1187137649571bb583f95a",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8e05f4ca2454734974ae90ad184c676c.js"
+  "./precache-manifest.ffb1c5bbb6ec03850da5d69e489f90d3.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
